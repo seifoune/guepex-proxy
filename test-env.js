3611d2b2dc7bcb8219e92f70ea016c87bbeba078
seifoune <89756632+seifoune@users.noreply.github.com>
2025-07-26 22:50:37 +0100

    Delete test-env.js

--- test-env.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-console.log('Test des variables d\'environnement:');
-console.log('GET_REDIRECT_URL:', process.env.GET_REDIRECT_URL);
-console.log('POST_REDIRECT_URL:', process.env.POST_REDIRECT_URL);
-console.log('PORT:', process.env.PORT); 
